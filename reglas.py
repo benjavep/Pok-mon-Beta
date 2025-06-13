297b0b214f9317d15b4c0567f92e7c47dd1a6c10
Benja_vep <benjamin.vega0208@gmail.com>
2025-06-13 12:18:41 -0300

    Update reglas.py

--- reglas.py
+++ reglas.py
@@ -11,27 +11,43 @@ pk = 0
 playlugarx = 130
 playlugary = 200
 
-fondo = pygame.image.load('/home/quinto/Escritorio/Benjamin Vega 5B 2025/prog/juegopokemon/reglass.png')
+lolo = pygame.image.load('/home/quinto/Escritorio/Benjamin Vega 5B 2025/prog/juegopokemon/reglass.png')
+
+def fade(screen, fondo, pk, playlugarx, playlugary): #esta definición la utilizamos para que haya una transicion para inciar el juego
+    fade_surface = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
+    fade_surface.fill((0, 0, 0))
+
+    #fundido a negro
+    for alpha in range(0, 255, 10):
+        fade_surface.set_alpha(alpha)
+        screen.blit(fondo, (0, 0))
+        screen.blit(fade_surface, (0, 0))
+        pygame.display.flip()
+        pygame.time.delay(30)
+
+    #cambiar a la siguiente escena
+    pokemonbenja.main(pk, playlugarx, playlugary)
+
 
 
 def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption("Introducción")
 
-    fondolu = pygame.transform.scale(fondo, (SCREEN_WIDTH, SCREEN_HEIGHT))
+    lolol = pygame.transform.scale(lolo, (SCREEN_WIDTH, SCREEN_HEIGHT)) #transformamos la escala de la imagen
 
     reloj = pygame.time.Clock()
-    funcionar = True
-    while funcionar:
+    lol = True
+    while lol:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                funcionar = False
+                lol = False
 
         tecla_p = pygame.key.get_pressed()
-        screen.blit(fondolu, (0, 0))
+        screen.blit(lolol, (0, 0))
 
         if tecla_p[pygame.K_e]:
-            pokemonbenja.main(pk, playlugarx, playlugary)
+            fade(screen, lolol, pk, playlugarx, playlugary)
 
         pygame.display.flip()
         reloj.tick(60)
